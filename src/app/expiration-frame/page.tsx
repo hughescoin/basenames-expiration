bc3da41ab80bda90aee51ac3f9731d9be87229e1
Patrick Hughes <patrick.hughes@coinbase.com>
2024-08-26 14:12:30 -0500

    implement frame using getFrameMetadata

--- src/app/expiration-frame/page.tsx
+++ src/app/expiration-frame/page.tsx
@@ -1,33 +1,48 @@
-import { FrameMetadata } from '@coinbase/onchainkit/frame';
+import { getFrameMetadata } from '@coinbase/onchainkit/frame';
+import type { Metadata } from 'next';
+import { NEXT_PUBLIC_URL } from '../../config';
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Story time!',
+    },
+    {
+      action: 'link',
+      label: 'Link to Google',
+      target: 'https://www.google.com',
+    },
+    {
+      label: 'Redirect to pictures',
+      action: 'post_redirect',
+    },
+  ],
+  image: {
+    src: `${NEXT_PUBLIC_URL}/basename-logo.png`,
+    aspectRatio: '1:1',
+  },
+  input: {
+    text: 'Tell me a boat story',
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/basename-expiration`,
+});
 
-export default function HomePage() {
-	return (
-		<FrameMetadata
-			buttons={[
-				{
-					label: 'Check expiration',
-				},
-				{
-					action: 'link',
-					label: 'Link to Google',
-					target: 'https://www.google.com',
-				},
-				{
-					action: 'post_redirect',
-					label: 'Redirect to cute pictures',
-				},
-			]}
-			image={{
-				src: 'https://zizzamia.xyz/park-3.png',
-				aspectRatio: '1:1',
-			}}
-			input={{
-				text: 'Tell me a boat story',
-			}}
-			state={{
-				counter: 1,
-			}}
-			postUrl="https://zizzamia.xyz/api/frame"
-		/>
-	);
+export const metadata: Metadata = {
+  title: 'Basename Expiration Tracker',
+  description: 'LFG',
+  openGraph: {
+    title: 'by Hughescoin',
+    description: 'LFG',
+    images: [`${NEXT_PUBLIC_URL}/basename-logo.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
+export default function Page() {
+  return (
+    <>
+      <h1>zizzamia.xyz</h1>
+    </>
+  );
 }
