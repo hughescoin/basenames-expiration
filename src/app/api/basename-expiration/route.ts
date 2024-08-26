ca86ac4dcf10f8cb0949a5b5e6702c2debcb1457
Patrick Hughes <patrick.hughes@coinbase.com>
2024-08-26 13:49:37 -0500

    api route responds with tokenId and expiration date

--- src/app/api/basename-expiration/route.ts
+++ src/app/api/basename-expiration/route.ts
@@ -1,61 +1,64 @@
-// pages/api/expiration.ts
-
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import { createPublicClient, getContract, http } from 'viem';
 import { base } from 'viem/chains';
 import {
-	BASENAMES_CONTRACT_ADDRESS,
-	unixToHumanReadable,
+  BASENAMES_CONTRACT_ADDRESS,
+  unixToHumanReadable,
 } from '../../../utils';
 import { BASENAMES_ABI } from '../../../abi';
 
 const publicClient = createPublicClient({
-	chain: base,
-	transport: http(),
+  chain: base,
+  transport: http(),
 });
 
 async function getNameExpiration(tokenId: bigint): Promise<string> {
-	const contract = getContract({
-		address: BASENAMES_CONTRACT_ADDRESS,
-		abi: BASENAMES_ABI,
-		client: publicClient,
-	});
+  const contract = getContract({
+    address: BASENAMES_CONTRACT_ADDRESS,
+    abi: BASENAMES_ABI,
+    client: publicClient,
+  });
 
-	const expirationTimeUnix = await contract.read.nameExpires([tokenId]);
-	let epochUnixTimestamp: bigint;
+  const expirationTimeUnix = await contract.read.nameExpires([tokenId]);
+  let epochUnixTimestamp: bigint;
 
-	if (typeof expirationTimeUnix === 'bigint') {
-		epochUnixTimestamp = expirationTimeUnix;
-	} else if (
-		typeof expirationTimeUnix === 'string' ||
-		typeof expirationTimeUnix === 'number'
-	) {
-		epochUnixTimestamp = BigInt(expirationTimeUnix);
-	} else {
-		throw new Error('Unexpected type for expirationTimeUnix');
-	}
+  if (typeof expirationTimeUnix === 'bigint') {
+    epochUnixTimestamp = expirationTimeUnix;
+  } else if (
+    typeof expirationTimeUnix === 'string' ||
+    typeof expirationTimeUnix === 'number'
+  ) {
+    epochUnixTimestamp = BigInt(expirationTimeUnix);
+  } else {
+    throw new Error('Unexpected type for expirationTimeUnix');
+  }
 
-	return unixToHumanReadable(epochUnixTimestamp);
+  return unixToHumanReadable(epochUnixTimestamp);
 }
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse
-): Promise<void> {
-	const { tokenId } = req.query;
+export async function GET(req: NextRequest) {
+  const searchParams = req.nextUrl.searchParams;
+  const tokenId = searchParams.get('tokenId');
+  console.log('params: ', searchParams);
+  console.log('tokenId2: ', tokenId);
 
-	if (!tokenId || typeof tokenId !== 'string') {
-		res
-			.status(400)
-			.json({ error: 'Token ID is required and must be a string' });
-		return;
-	}
-
-	try {
-		const readableDate = await getNameExpiration(BigInt(tokenId));
-		res.status(200).json({ tokenId, expiration: readableDate });
-	} catch (error) {
-		console.error('Error fetching expiration:', error);
-		res.status(500).json({ error: 'Internal Server Error' });
-	}
+  //   if (!tokenId || typeof tokenId !== 'string') {
+  //     return new Response("error: 'Token ID is required and must be a string'", {
+  //       status: 400,
+  //     });
+  //   }
+  if (!tokenId) {
+    return Response.json({ error: 'Token ID is required' }, { status: 400 });
+  }
+  try {
+    const readableDate = await getNameExpiration(BigInt(tokenId));
+    console.log(`api data: \n`, { tokenId, expiration: readableDate });
+    return NextResponse.json({ tokenId, expiration: readableDate });
+  } catch (error) {
+    console.error('Error fetching expiration:', error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
 }
