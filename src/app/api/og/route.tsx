f175e8c9a36a7e1ae160e43fd3bd4108bce0a1df
Patrick Hughes <patrick.hughes@coinbase.com>
2024-08-26 17:48:39 -0500

    handle wrong tokenIds using generateErrorImage

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -2,6 +2,42 @@ import { ImageResponse } from 'next/og';
 import { getNameExpiration } from '../../../lib/basenames';
 import { NextRequest } from 'next/server';
 
+function isValidTokenId(tokenId: string): boolean {
+  return /^\d+$/.test(tokenId);
+}
+
+function generateErrorImage(tokenId: string) {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: 'black',
+          background: 'white',
+          width: '100%',
+          height: '100%',
+          padding: '50px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1 style={{ fontSize: 60, marginBottom: 20, color: '#3b82f6' }}>
+          Basename Expiration
+        </h1>
+        <p style={{ fontSize: 30, marginBottom: 10 }}>
+          Token ID: {tokenId} is invalid 😢
+        </p>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    }
+  );
+}
+
 export async function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const tokenId = searchParams.get('tokenId');
@@ -11,39 +47,15 @@ export async function GET(req: NextRequest) {
     return new Response('Token ID is required', { status: 400 });
   }
 
+  if (!isValidTokenId(tokenId)) {
+    return generateErrorImage(tokenId);
+  }
+
   try {
     const readableDate = await getNameExpiration(BigInt(tokenId));
+
     if (readableDate === '0') {
-      return new ImageResponse(
-        (
-          <div
-            style={{
-              fontSize: 40,
-              color: 'black',
-              background: 'white',
-              width: '100%',
-              height: '100%',
-              padding: '50px',
-              display: 'flex',
-              flexDirection: 'column',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-          >
-            <h1 style={{ fontSize: 60, marginBottom: 20, color: '#3b82f6' }}>
-              Basename Expiration
-            </h1>
-            <p style={{ fontSize: 30, marginBottom: 10 }}>
-              Token ID: {tokenId} is invalid 😢
-              <span style={{ fontWeight: 'bold' }}></span>
-            </p>
-          </div>
-        ),
-        {
-          width: 1200,
-          height: 630,
-        }
-      );
+      return generateErrorImage(tokenId);
     }
 
     return new ImageResponse(
